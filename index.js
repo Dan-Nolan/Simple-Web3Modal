ce5482c9302eebd1fa7d3051cc0f421413d55ed6
Daniel Nolan <DanNolan99@gmail.com>
2021-03-22 12:42:42 -0400

    adds basic portis integration

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 import Web3Modal from "web3modal";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-
-const providerOptions = {
-  walletconnect: {
-    package: WalletConnectProvider,
-    options: {
-      // testkey
-      infuraId: "8043bb2cf99347b1bfadfb233c5325c0",
-    }
-  }
-};
+import {ethers} from 'ethers';
+import renderAccount from './src/renderAccount';
+import providerOptions from './src/providerOptions';
 
 const web3Modal = new Web3Modal({
-  network: "mainnet", // optional
-  cacheProvider: true, // optional
-  providerOptions // required
+  network: "mainnet",
+  cacheProvider: true,
+  providerOptions
 });
 
-web3Modal.connect();
+async function render() {
+  const provider = await connect();
+
+  const signer = await provider.getSigner(0);
+
+  const address = await signer.getAddress();
+  const rawBalance = await provider.getBalance(address);
+  const balance = ethers.utils.formatEther(rawBalance);
+
+  renderAccount({ address, balance }, logoutHandler);
+}
+
+async function logoutHandler() {
+  web3Modal.clearCachedProvider();
+  await connect();
+  await render();
+}
+
+async function connect() {
+  const externalProvider = await web3Modal.connect();
+  return new ethers.providers.Web3Provider(externalProvider);
+}
+
+render();
---
+++ src/providerOptions.js
@@ -0,0 +1,12 @@
+import Portis from "@portis/web3";
+
+const providerOptions = {
+  portis: {
+    package: Portis, 
+    options: {
+      id: "bda38cc8-d135-4d4d-bc96-e15ce7ad6da5"
+    }
+  }
+};
+
+export default providerOptions;
