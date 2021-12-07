c8c4982ff05bcf8c4b98bc09d3cded584c4388a0
Dan <DanNolan99@gmail.com>
2021-12-07 11:37:01 -0500

    update to parcel v2 & add walletconnect

--- src/providerOptions.js
+++ src/providerOptions.js
@@ -1,10 +1,17 @@
 import Portis from "@portis/web3";
+import WalletConnectProvider from "@walletconnect/web3-provider";
 
 const providerOptions = {
   portis: {
     package: Portis, 
     options: {
-      id: "bda38cc8-d135-4d4d-bc96-e15ce7ad6da5"
+      id: process.env.PORTIS_ID
+    }
+  },
+  walletconnect: {
+    package: WalletConnectProvider,
+    options: {
+      infuraId: process.env.INFURA_ID
     }
   }
 };
